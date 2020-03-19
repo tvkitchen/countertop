254a26ce8dad1d3f66b849a14a6865f4703cb10b
Dan Schultz <slifty@gmail.com>
2020-03-19 00:14:59 -0400

    Add aliases to eslint configuration
    This re-uses the moduleAliases package.json configuration so that we
    don't have to specify module aliases in two places.

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+const package = require('./package.json')
+
+const extractModuleAliasesFromPackage = (package) => {
+	const aliasStems = Object.keys(package._moduleAliases)
+	return aliasStems.map(
+		(stem) => [stem, package._moduleAliases[stem]]
+	)
+}
+const aliases = extractModuleAliasesFromPackage(package)
+const settings = {
+	"extends": "airbnb",
+	"parser": "babel-eslint",
+	"rules": {
+		"semi": [2, "never"],
+		"indent": [2, "tab"],
+		"no-tabs": 0
+	},
+	"env": {
+		"es6": true,
+		"node": true,
+		"jest": true
+	},
+	"settings": {
+		"react": {
+			"version": "latest"
+		},
+		"import/resolver": {
+			"alias": aliases
+		}
+	}
+}
+
+module.exports = settings
