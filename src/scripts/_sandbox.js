5f2560d6f857beefd9bdb252adf10bc682af7a8a
Dan Schultz <slifty@gmail.com>
2020-04-11 11:41:23 -0400

    Add sandbox script
    The sandbox script makes it easy to test local changes and tweaks
    without having to worry about managing ad-hoc local test scripts.  It
    provides instructions to users of the script on how to ensure they don't
    accidentally commit their local tests.

---
+++ src/scripts/_sandbox.js
@@ -0,0 +1,11 @@
+// This file is for testing out local changes easily.
+//
+// To run this code, type `yarn sandbox` in your CLI.
+//
+// Do not commit changes to this file! In fact, you should locally set the `--assume-unchanged`
+// flag on it: `git update-index --assume-unchanged ./src/scripts/_sandbox.js`
+import 'module-alias/register'
+
+/**
+ * DO NOT MODIFY ABOVE THIS BLOCK
+ */
