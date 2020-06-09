ff61ea8611d092740e38a020b06d7aff4cd979f1
Chris Zubak-Skees <chriszs@gmail.com>
2020-06-08 21:56:46 -0400

    Silence console output during tests
    This silences console output during tests,
    including from Winston's console transport,
    which required a Jest setup file to accomplish
    because Winston and Jest don't play nice.
    Can be overridden by specifying --no-silent.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ const { getModuleAliasesForJest } = require('./lib/utils/moduleAlias')
 
 const settings = {
 	moduleNameMapper: getModuleAliasesForJest(),
+	setupFiles: ['./lib/utils/jest.setup.js'],
+	silent: true,
 }
 
 module.exports = settings
