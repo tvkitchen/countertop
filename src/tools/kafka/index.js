38e5c4e66698679882183f0c5aa71a37fd7c55c6
Dan Schultz <slifty@gmail.com>
2020-09-09 11:03:22 -0400

    Remove old kafka
    These kafka objects are now obsolete, and are not used by the
    Countertop.
    
    This takes out the old kafka, the old environment variable setting, and
    also removes some of the utility scripts for spinning up a local kafka
    instance.
    
    It keeps the travis support for kafka, although it is still not being
    used.  This is because integration tests will require it and the
    repository was supporting travis kafka before.
    
    Issue #99

--- src/tools/kafka/index.js
+++
@@ -1,9 +0,0 @@
-import { Kafka } from 'kafkajs'
-import config from '../../config'
-
-const kafka = new Kafka({
-	clientId: 'tv-kitchen',
-	brokers: [config.KAFKA_BROKERS],
-})
-
-export default kafka
