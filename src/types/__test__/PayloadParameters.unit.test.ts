b60d311ab24fd6c2645baf52fad683fb7ff7aeac
Dan Schultz <slifty@gmail.com>
2022-06-20 17:25:36 -0400

    Use Ajv for PayloadParameter validation
    This is our first use of Ajv to define a TypeGuard; it seems to work
    well.
    
    This might seem a bit redundant but I think that's just the nature of
    the fact that TypeScript interfaces are not compiled to any sort of
    runtime environment.
    
    It also adds tests for the TypeGuard which should have been included
    originally.

---
+++ src/types/__test__/PayloadParameters.unit.test.ts
@@ -0,0 +1,103 @@
+import { isPayloadParameters } from '../PayloadParameters'
+
+describe('isPayloadParameters', () => {
+	it('should return true for valid payload parameters', () => {
+		expect(isPayloadParameters({
+			data: Buffer.from('I ate all of the cheese'),
+			type: 'CONFESSION',
+			createdAt: '2020-02-02T03:04:05.000Z',
+			origin: '2020-02-02T03:04:01.000Z',
+			duration: 1000,
+			position: 60000,
+		})).toEqual(true)
+	})
+	it('should return true with no createdAt parameter', () => {
+		expect(isPayloadParameters({
+			data: Buffer.from('I ate all of the cheese'),
+			type: 'CONFESSION',
+			origin: '2020-02-02T03:04:01.000Z',
+			duration: 1000,
+			position: 60000,
+		})).toEqual(true)
+	})
+	it('should return false for incomplete payload parameters', () => {
+		expect(isPayloadParameters({
+			type: 'CONFESSION',
+			createdAt: '2020-02-02T03:04:05.000Z',
+			origin: '2020-02-02T03:04:01.000Z',
+			duration: 1000,
+			position: 60000,
+		})).toEqual(false)
+	})
+	it('should return false for overly populated payload parameters', () => {
+		expect(isPayloadParameters({
+			data: Buffer.from('I ate all of the cheese'),
+			type: 'CONFESSION',
+			createdAt: '2020-02-02T03:04:05.000Z',
+			origin: '2020-02-02T03:04:01.000Z',
+			duration: 1000,
+			position: 60000,
+			notReallyUsed: 42,
+		})).toEqual(false)
+	})
+	it('should return false for incorrectly typed payload parameters', () => {
+		expect(isPayloadParameters({
+			data: 'this is not a buffer',
+			type: 'CONFESSION',
+			createdAt: '2020-02-02T03:04:05.000Z',
+			origin: '2020-02-02T03:04:01.000Z',
+			duration: 1000,
+			position: 60000,
+		})).toEqual(false)
+		expect(isPayloadParameters({
+			data: {
+				field: 'this is an object, but not a buffer.',
+			},
+			type: 'CONFESSION',
+			createdAt: '2020-02-02T03:04:05.000Z',
+			origin: '2020-02-02T03:04:01.000Z',
+			duration: 1000,
+			position: 60000,
+		})).toEqual(false)
+		expect(isPayloadParameters({
+			data: Buffer.from('I ate all of the cheese'),
+			type: 42,
+			createdAt: '2020-02-02T03:04:05.000Z',
+			origin: '2020-02-02T03:04:01.000Z',
+			duration: 1000,
+			position: 60000,
+		})).toEqual(false)
+		expect(isPayloadParameters({
+			data: Buffer.from('I ate all of the cheese'),
+			type: 'CONFESSION',
+			createdAt: 0,
+			origin: '2020-02-02T03:04:01.000Z',
+			duration: 1000,
+			position: 60000,
+		})).toEqual(false)
+		expect(isPayloadParameters({
+			data: Buffer.from('I ate all of the cheese'),
+			type: 'CONFESSION',
+			createdAt: '2020-02-02T03:04:05.000Z',
+			origin: 0,
+			duration: 1000,
+			position: 60000,
+		})).toEqual(false)
+		expect(isPayloadParameters({
+			data: Buffer.from('I ate all of the cheese'),
+			type: 'CONFESSION',
+			createdAt: '2020-02-02T03:04:05.000Z',
+			origin: '2020-02-02T03:04:01.000Z',
+			duration: '1000',
+			position: 60000,
+		})).toEqual(false)
+		expect(isPayloadParameters({
+			data: Buffer.from('I ate all of the cheese'),
+			type: 'CONFESSION',
+			createdAt: '2020-02-02T03:04:05.000Z',
+			origin: '2020-02-02T03:04:01.000Z',
+			duration: 1000,
+			position: '60000',
+		})).toEqual(false)
+	})
+})
