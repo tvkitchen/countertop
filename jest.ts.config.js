d5a36daf3611112f4cb16d36099a6427a86e0b84
Dan Schultz <slifty@gmail.com>
2023-07-24 22:28:02 -0400

    Fix jest typescript configuration
    Jest was still using ts-jest to process typescript tests, which can
    cause problems in cases where the tests interacted with javascript
    modules.
    
    This removes that in favor of using our babel config.  Once we're fully
    typescript we can remove babel and re-add ts-jest.

--- jest.ts.config.js
+++ jest.ts.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-	globals: {
-		"ts-jest": {
-			tsconfig: 'tsconfig.dev.json',
-		},
-	},
-	preset: 'ts-jest',
 	testEnvironment: 'node',
 	testPathIgnorePatterns: [
 		"<rootDir>/dist/",
